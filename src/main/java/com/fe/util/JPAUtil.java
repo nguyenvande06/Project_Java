c74d4f0bcec2066c08b9990d45f1c2bc02f5655a
Pham Minh Quan <qmarch1555@gmail.com>
2026-06-27 09:29:08 +0700

    feat: implement nghiep vu 5 - Skill Gap Analysis (phan tich khoang cach ky nang)

---
+++ src/main/java/com/fe/util/JPAUtil.java
@@ -0,0 +1,16 @@
+package com.fe.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	private static EntityManagerFactory emf;
+
+	public static EntityManager getEntityManager() {
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("JPAs");
+		}
+		return emf.createEntityManager();
+	}
+}
